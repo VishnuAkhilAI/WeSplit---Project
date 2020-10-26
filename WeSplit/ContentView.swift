c23cf248b23d3143a2544f3b43a1a8768709e071
Vishnu Akhil <vishnu.akhil10@gmail.com>
2020-10-27 07:09:48 +1100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Vishnu akhil Upparapalle on 27/10/20.
+//  Copyright © 2020 Vishnu akhil Upparapalle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
